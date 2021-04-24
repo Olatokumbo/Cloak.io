fd5f88cef66383c0c36c8810eb235290fc73cf96
Olatokumbo <faithodesola@gmail.com>
2021-04-25 00:06:03 +0100

    Designing the Main page and navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,49 @@
-const Navbar = () =>{
-    return(
-        <nav>
-            <h1>cloak.io</h1>
-        </nav>
-    )
-}
+import { useState } from "react";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="flex flex-col justify-between items-center p-5 top-0 left-0 right-0 sm:flex-row w-screen">
+      <div className="flex w-full justify-between items-center">
+        <h1 className="text-2xl font-light hover: cursor-pointer">cloak.io</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 lg:hidden md:hidden sm:hidden cursor-pointer block"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          htmlFor="menu-toggle"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16m-7 6h7"
+          />
+        </svg>
+      </div>
+      <div
+        id="menu"
+        className={
+          "items-center sm:flex md:flex" + (isOpen ? " flex" : " hidden")
+        }
+      >
+        <ul className="flex flex-col list-none lg:ml-auto items-center justify-center sm:flex-row">
+          <li className="sm:my-0 my-1">
+            <h1 className="mx-4 hover: cursor-pointer">Contact</h1>
+          </li>
+          <li className="sm:my-0 my-1">
+            <h1 className="mx-4 hover: cursor-pointer">Login</h1>
+          </li>
+          <li className="sm:my-0 my-1">
+            <button className="mx-4 bg-black text-white py-1.5 px-2.5 rounded-md hover:bg-gray-900 focus:outline-none">
+              Signup
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -8,9 +9,33 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar />
       <main className={styles.main}>
-        <h1 className="text-red-600">Hello</h1>
+        <div className="flex flex-col my-9">
+          <h1 className="text-5xl font-bold sm:text-7xl md:text-9xl">Hire a</h1>
+          <h1 className="text-5xl font-bold sm:text-7xl md:text-9xl bg-gradient-to-r from-pink-500 via-blue-500 to-indigo-900 bg-clip-text text-transparent ">
+            Developer
+          </h1>
+        </div>
+        <div className="flex flex-col items-center">
+          <h5 className="text-center max-w-md text-sm px-3 md:max-w-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </h5>
+          <div className="flex mx-auto mt-16">
+            <button className="mx-5 bg-black focus:outline-none text-white px-3 py-2 md:px-4 rounded-md hover:bg-gray-900">
+              Find Freelancers
+            </button>
+            <button className="mx-5 focus:outline-none px-2 py-2 sm:px-4 sm:py-2 md:px-4 border-black border-solid border-4 rounded-md hover:bg-gray-200">
+              Join the Force
+            </button>
+          </div>
+          <h6 className="my-3 text-gray-700 text-xs">
+            Lorem ipsum dolor sit amet, consectetur
+          </h6>
+        </div>
       </main>
     </div>
   );
