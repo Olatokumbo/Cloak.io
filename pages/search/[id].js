824d4cf4dca10322ceac1ab9e9a6b1048b2a8a4c
Olatokumbo <faithodesola@gmail.com>
2021-04-29 23:46:55 +0100

    Some layout changes

--- pages/search/[id].js
+++ pages/search/[id].js
@@ -8,9 +8,12 @@ const Profile = () => {
       <div className="flex">
         <div className="flex-none lg:flex-1 bg-gray-200"></div>
         <div className="flex-3 bg-white py-5 px-10">
+          <div className="flex justify-between items-center">
           <h1 className="text-2xl font-semibold my-5">
             Full Stack Web Developer
           </h1>
+          <h5 className="text-2xl font-semibold text-gray-800">₦35,000+</h5>
+          </div>
           <div className="flex justify-between mb-7">
             <div className="flex items-center">
               <img
@@ -22,13 +25,31 @@ const Profile = () => {
                 <h4 className="ext-base font-bold text-gray-800">
                   faithodesola
                 </h4>
-                <h6 className="text-xs text-gray-600">Level 2</h6>
+                <div className="flex">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 text-gray-500"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+
+                  <h5 className="text-xs self-end text-gray-500">
+                    Ogun, Lagos
+                  </h5>
+                </div>
               </div>
             </div>
-            <button className="mx-5 bg-black focus:outline-none text-white px-3 py-2 md:px-4 rounded-md hover:bg-gray-900">
-              Contact Me
+            <button className="ml-5 bg-black focus:outline-none text-white px-3 py-2 md:px-4 rounded-md hover:bg-gray-900">
+              Get in Touch
             </button>
           </div>
+          <h6 className="text-md text-gray-600">Level 2</h6>
           <h1 className="text-lg font-semibold mt-5">My Works</h1>
           <ProfileCarousel />
           <h1 className="font-bold text-lg my-5 text-gray-800">Description</h1>
