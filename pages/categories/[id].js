5587a9679e4daa4b9bf37fd0de2c9d491bfa46d9
Olatokumbo <faithodesola@gmail.com>
2021-05-04 07:27:50 +0100

    Dynamic titles for the Category Pages

---
+++ pages/categories/[id].js
@@ -0,0 +1,92 @@
+import Image from "next/image";
+import CategoryList from "../../sections/CategoryList";
+import ProfileCard from "../../components/ProfileCard";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import fs from "fs";
+import path from "path";
+
+const Category = ({ category }) => {
+  return (
+    <Layout>
+      <CategoryList />
+      <div className="w-full relative overflow-hidden mb-9">
+        <div className="opacity-50 bg-gray-900 absolute left-0 right-0 top-0 bottom-0"></div>
+        <div className="absolute p-10 flex h-full w-full">
+          <div className="flex-1 flex flex-col justify-center items-start">
+            <h1 className="text-white text-3xl font-semibold">
+              {category.title}
+            </h1>
+          </div>
+          <div className="flex-none md:flex-1"></div>
+        </div>
+        <div className="flex-1 relative -z-1 h-40">
+          <Image
+            src={category.photoURL}
+            className="object-cover"
+            layout="fill"
+          />
+        </div>
+      </div>
+      <div className="mb-5 w-full px-2 grid gap-x-2 gap-y-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5">
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+        <Link href="/search/12">
+          <a>
+            <ProfileCard />
+          </a>
+        </Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default Category;
+
+export const getStaticPaths = async () => {
+  const filePath = path.join(process.cwd(), "utils", "category.json");
+  let fileData = fs.readFileSync(filePath);
+  fileData = JSON.parse(fileData);
+  let paths = fileData.map((data) => ({
+    params: { id: data.id },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const filePath = path.join(process.cwd(), "utils", "category.json");
+  let fileData = fs.readFileSync(filePath);
+  fileData = JSON.parse(fileData);
+  const category = fileData.filter((data) => data.id === params.id)[0];
+  return {
+    props: {
+      category,
+    },
+  };
+};
