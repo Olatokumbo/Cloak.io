280158442defcf4f8d45330b6fe89e00b18fc4ec
Olatokumbo <faithodesola@gmail.com>
2021-05-04 10:42:08 +0100

    Added Links to the Explore tiles

--- components/ExploreTile.js
+++ components/ExploreTile.js
@@ -1,6 +1,6 @@
 const ExploreTile = ({title, src}) => {
   return (
-    <div className="my-5 mx-1 sm:mx-2 p-7 flex flex-col justify-center rounded-md cursor-pointer">
+    <div className="my-5 mx-1 sm:mx-2 p-7 flex flex-col justify-center rounded-md cursor-pointer hover:bg-gray-100 transition duration-200 ease-in-out">
       <img className="w-12 h-auto m-auto" src={src} alt="design" />
       <h5 className="font-semibold text-gray-600 text-sm text-center">{title}</h5>
     </div>
--- sections/Explore.js
+++ sections/Explore.js
@@ -1,17 +1,57 @@
+import Link from "next/link";
 import ExploreTile from "../components/ExploreTile";
 const Explore = () => {
   return (
     <div className="w-full mt-28">
       <h1 className="text-3xl font-bold text-gray-800">Explore</h1>
       <div className="my-4 grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 w-full">
-        <ExploreTile src="/icons/design.svg" title="Graphics and Design" />
-        <ExploreTile src="/icons/tech.svg" title="Technology" />
-        <ExploreTile src="/icons/audio.svg" title="Audio and Music" />
-        <ExploreTile src="/icons/business.svg" title="Business" />
-        <ExploreTile src="/icons/marketing.svg" title="Marketing" />
-        <ExploreTile src="/icons/training.svg" title="Education and Training" />
-        <ExploreTile src="/icons/media.svg" title="Media and Communication" />
-        <ExploreTile src="/icons/health.svg" title="Health" />
+        <Link href="/categories/graphics-and-design">
+          <a>
+            <ExploreTile src="/icons/design.svg" title="Graphics and Design" />
+          </a>
+        </Link>
+        <Link href="/categories/technology">
+          <a>
+            <ExploreTile src="/icons/tech.svg" title="Technology" />
+          </a>
+        </Link>
+        <Link href="/categories/audio-and-music">
+          <a>
+            <ExploreTile src="/icons/audio.svg" title="Audio and Music" />
+          </a>
+        </Link>
+        <Link href="/categories/business">
+          <a>
+            <ExploreTile src="/icons/business.svg" title="Business" />
+          </a>
+        </Link>
+        <Link href="/categories/marketing">
+          <a>
+            {" "}
+            <ExploreTile src="/icons/marketing.svg" title="Marketing" />
+          </a>
+        </Link>
+        <Link href="/categories/education-and-training">
+          <a>
+            <ExploreTile
+              src="/icons/training.svg"
+              title="Education and Training"
+            />
+          </a>
+        </Link>
+        <Link href="/categories/media-and-communication">
+          <a>
+            <ExploreTile
+              src="/icons/media.svg"
+              title="Media and Communication"
+            />
+          </a>
+        </Link>
+        <Link href="/categories/health">
+          <a>
+            <ExploreTile src="/icons/health.svg" title="Health" />
+          </a>
+        </Link>
       </div>
     </div>
   );
