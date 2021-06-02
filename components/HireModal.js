9999e856d391b5e170fa76c81e50bb0ad3d1ad20
David Faith <faithodesola@gmail.com>
2021-06-02 12:15:23 +0100

    Added the Hire Me Modal

---
+++ components/HireModal.js
@@ -0,0 +1,146 @@
+import { useState, useEffect } from "react";
+import {
+  Modal,
+  Fade,
+  Backdrop,
+  Button,
+  makeStyles,
+  TextField,
+} from "@material-ui/core";
+// import { deleteJob } from "../redux/actions/jobs";
+import { useRouter } from "next/router";
+import { set } from "date-fns";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    // position: "absolute",
+    transform: `translate(0%, 20%)`,
+    minWidth: 180,
+    maxWidth: 400,
+    backgroundColor: theme.palette.background.paper,
+    // border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+  },
+  btn: {
+    backgroundColor: "#374151",
+    "&:hover": {
+      background: "#535e70",
+    },
+  },
+}));
+const DeleteModal = ({ open, handleClose, id, data }) => {
+  const classes = useStyles();
+  const router = useRouter();
+  const [title, setTitle] = useState(data.title);
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(data.price);
+
+  const hire = async () => {
+    // try {
+    //   await deleteJob(id);
+    //   router.replace("/job/all");
+    // } catch (error) {
+    //  alert(error.message)
+    // }
+    handleClose();
+  };
+
+  useEffect(() => {
+    setTitle(data.title);
+    setPrice(data.price)
+  }, [open]);
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={open}
+      onClose={handleClose}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={open}>
+        <div className={classes.paper}>
+          <h1 className="text-gray-700 font-semibold">Hire Form</h1>
+          <h5 className="text-gray-700 font-light my-3 text-sm">
+            *Please contact the seller before filling this form*
+          </h5>
+          <div>
+            <TextField
+              name="Title"
+              size="small"
+              label="Title"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              onChange={(e) => setTitle(e.target.value)}
+              autoComplete="off"
+              required
+              value={title}
+            />
+            <TextField
+              type="number"
+              name="price"
+              size="small"
+              label="Price"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              //   InputProps={{ inputProps: { min: data.price} }}
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              required
+            />
+            <TextField
+              name="description"
+              size="small"
+              label="Description"
+              variant="outlined"
+              fullWidth={true}
+              margin="dense"
+              multiline={true}
+              rows={5}
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+            />
+          </div>
+          <div
+            // onSubmit={updateDescriptionHandler}
+            className="flex flex-col w-full"
+          >
+            <div className="w-full flex justify-between">
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                margin="dense"
+                onClick={handleClose}
+                // className={classes.btn}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                margin="normal"
+                className={classes.btn}
+                onClick={handleDeleteJob}
+              >
+                Done
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
--- pages/search/[id].js
+++ pages/search/[id].js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import HireModal from "../../components/HireModal";
 import Layout from "../../components/Layout";
 import ProfileCarousel from "../../components/ProfileCarousel";
 import ProfileComment from "../../components/ProfileComment";
@@ -9,6 +10,14 @@ import { useSelector } from "react-redux";
 const Profile = ({ poster }) => {
   const uid = useSelector((state) => state.auth.uid);
   const [buttonState, setButtonState] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   if (!poster)
     return (
       <Layout>
@@ -21,7 +30,7 @@ const Profile = ({ poster }) => {
     else alert("Please Signin First");
   };
   const hireMe = () => {
-    if (uid) console.log("Hire Me");
+    if (uid) handleOpen();
     else alert("Please Signin First");
   };
   return (
@@ -110,6 +119,7 @@ const Profile = ({ poster }) => {
         </div>
         <div className="flex-none lg:flex-1 bg-gray-200"></div>
       </div>
+      <HireModal open={open} handleClose={handleClose} id={uid} data={{title: poster.title, price: poster.price}}/>
     </Layout>
   );
 };
